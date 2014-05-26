aa45b63331c6f8427588126e487b640fc0b6c260
KoryKozlowski <kdkzlwsk@memphis.edu>
2014-05-26 16:13:15 -0500

    File upload/validation recipes

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,10 @@
 <?php
 
+Route::get('/', function()
+{
+   return View::make('hello');   
+});
+
 Route::get('userresults', function()
 {
    // die & dump validated input
@@ -27,4 +32,36 @@ Route::post('userform', function()
    }
 
    return Redirect::to('userresults')->withInput();
+});
+
+Route::get('fileform', function()
+{
+   return View::make('fileform');
+});
+
+Route::post('fileform', function()
+{
+   $rules = array(
+      'myfile' => 'mimes:doc,docx,pdf,txt | max:1000'
+   );
+
+   $validation = Validator::make(Input::all(), $rules);
+
+   if($validation->fails())
+   {
+      return Redirect::to('fileform')->withErrors($validation)->withInput();
+   }
+   else
+   {
+      $file = Input::file('myfile');
+
+      if($file->move('files', $file->getClientOriginalName()))
+      {
+         return 'Success';
+      }
+      else
+      {
+        return 'Error';
+      }
+   }
 });
